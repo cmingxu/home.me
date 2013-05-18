a615a6efceee82f3aa69e6ea1ec1f3f567e6092d
cming.xu <cming.xu@gmail.com>
2013-05-18 13:09:38 +0800

    thin support

---
+++ Rakefile
@@ -0,0 +1,3 @@
+task :default do 
+  system "thin start -d -p 4567"
+end
--- index.rb
+++ index.rb
@@ -1,10 +1,12 @@
 require "sinatra"
 DATA_PATH = File.expand_path(File.join(File.dirname(__FILE__), "data"))
 
-get '/get' do
-  File.read(DATA_PATH)
-end
+class App < Sinatra::Base
+  get '/get' do
+    File.read(DATA_PATH)
+  end
 
-post '/set' do
-  File.write(DATA_PATH, params[:address])
+  post '/set' do
+    File.write(DATA_PATH, params[:address])
+  end
 end
